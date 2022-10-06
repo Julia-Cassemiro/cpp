24d1c674a04be12d81d9210f7481eccb002b030a
Julia Cassemiro <jg-cassemiro@hotmail.com>
2022-10-06 19:54:03 +0200

    ex01

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 09:47:49 by jgomes-c          #+#    #+#             */
-/*   Updated: 2022/09/01 09:47:49 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2022/09/01 17:29:44 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,24 @@
 
 #include <string>
 #include <iostream>
-#include <locate>
+// #include <locate>
 
 int main(int argc, char const *argv[]){
+
+
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl; //printa
+	else
+	{
+		for (int count = 1; count < argc; count++)
+		{
+			std::string str(argv[count]); 	//	sequência de caracteres como um objeto de classe . 
+											//	Esta classe é chamada std::string. A classe String armazena os caracteres como uma sequência de bytes
+											// 	com a funcionalidade de permitir o acesso a um caractere de byte único .
+			for (size_t i = 0; i < str.length(); i++)
+				std::cout << (char)std::toupper(str[i]); //passa cada letra para maiusculo, e printa
+		}
+		std::cout << std::endl; //quebra de linha
+	}
+	return (0);
 }
